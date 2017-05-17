84fa75c0c468f9d397859abc2fab363eb333e27b
huyqut <huy.qut@gmail.com>
2017-05-17 07:51:04 +0700

    Chapter 20: sys module

---
+++ python/python-101-201/python_101/chap20_sys.py
@@ -0,0 +1,18 @@
+# "sys" module
+
+import sys
+
+
+print(sys.argv)  # Arguments passed to this python program
+print(sys.executable)  # Path to Python interpreter
+print(sys.copyright)  # Copyright info of Python interpreter
+print(sys.version)  # Version of this interpreter
+print(sys.version_info)  # Info of version
+print(sys.api_version)  # API version
+print(sys.path)  # Search path for modules
+print(sys.platform)  # Platform in which the program is run
+print(sys.stdin)
+print(sys.stdout)
+print(sys.stderr)
+
+sys.exit(0)  # Exit program with status
