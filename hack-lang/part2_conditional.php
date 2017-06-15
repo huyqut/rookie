7305b22f26e3e214199dffc4766402bc0d9a070a
huyqut <huy.qut@gmail.com>
2017-06-15 21:45:09 +0700

    Hack and HHVM: Conditional statements

---
+++ hack-lang/part2_conditional.php
@@ -0,0 +1,37 @@
+<?hh
+
+//  Simple `if`
+if (true) {
+    pra("It must be true");
+}
+$a = true;
+$b = 10;
+$c = 9.99;
+//  `if` with `else`
+if ($a && !($b == 10 || ($c < 100.0 ^ false))) {
+    pra("I'm not sure if this is right");
+} else {
+    pra("Yo! It must be wrong");
+}
+
+//  `elseif`
+if ($a == false) {
+    pra("Not this one");
+} elseif ($b < 9) {
+    pra("Not this one also");
+} else {
+    pra("Yup! This is it! Eat this shit!");
+}
+
+//  `switch`
+switch ($b) {
+    case 100:
+        pra("Yaya");
+        break;
+    case 1:
+        pra("Papa");
+        break;
+    default:
+        pra("Yolo");
+        break;
+}
